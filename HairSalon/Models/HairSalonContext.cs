34797e0a988150cdf277a4fc1bf046e2a3d65c0e
Justin Lee <lee.justin001126@gmail.com>
2023-03-10 10:12:57 -0800

    fixed bug with routing and context Models file

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-namespace BestRestaurant.Models;
 
-public class HairSalonContext : DbContext
-{
-  public DbSet<Stylist> Stylists { get; set; }
-  public DbSet<Client> Clients { get; set; }
 
-  public HairSalonContextContext(DbContextOptions options) : base(options) { }
+namespace HairSalon.Models{
+  public class HairSalonContext : DbContext
+  {
+    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
+
+    public HairSalonContext(DbContextOptions options) : base(options) { }
+  }
 }
