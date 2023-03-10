7388f31280db78a60364fe408f1821aa3fa30633
Justin Lee <lee.justin001126@gmail.com>
2023-03-10 13:04:08 -0800

    Fixed bugs regarding routing once again

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -38,14 +38,14 @@ namespace HairSalon.Controllers
       }
       _db.Clients.Add(client);
       _db.SaveChanges();
-      return RedirectToaction("Index");
+      return RedirectToAction("Index");
     
     }
 
     public ActionResult Details(int id)
     {
       Client thisClient = _db.Clients.Include(client => client.Stylist).FirstOrDefault(client => client.ClientId == id);
-      return view(thisClient);
+      return View(thisClient);
     }
 
   }
