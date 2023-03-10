a58379f591be50ab7344801b7fc750d488948673
Justin Lee <lee.justin001126@gmail.com>
2023-03-10 15:38:56 -0800

    WIP working through bugs in deleting a stylist

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -39,6 +39,21 @@ namespace HairSalon.Controllers
       Stylist thisStylist = _db.Stylists.Include(stylist => stylist.Clients).FirstOrDefault(stylist => stylist.StylistId == id);
       return View(thisStylist);
     }
+
+    public ActionResult Delete(int id)
+    {
+      Stylist thisStylist = _db.Clients.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Stylist thisStylist = _db.Clients.FirstOrDefault(stylist => stylist.ClientId == id);
+      _db.Stylist.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
 
